f633a1c9cc67f164d9eae787f6ca6caf614f4e20
Mirko <ruhlmirkojoel@gmail.com>
2024-11-02 02:33:20 -0300

    Updated gitignore and changed cells generation

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import fpdf
-from fpdf.enums import XPos, YPos
+import pandas as pd
 
 pdf = fpdf.FPDF(orientation='P', unit='mm', format='A4')
+df = pd.read_csv('topics.csv')
+
+pdf.set_font('Times', 'B', 20)
+
 
 pdf.add_page()
+for index, row in df.iterrows():
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(h=10, text=row['Topic'], new_x="LMARGIN", new_y="NEXT")
+    pdf.line(10, 20, 200, 20)
+    # First
+
+    for page in range(int(row['Pages'])):
+        pdf.add_page()
 
 pdf.output('output.pdf')
